a003ec084a4c6e59b31d26ce16c8e4a5ab78b8a8
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-06-20 11:15:12 +0545

    Done

--- public/js/infiniteScroll.js
+++ public/js/infiniteScroll.js
@@ -6,8 +6,7 @@ async function appendBlog() {
         const data = await response.json();
         const posts = data.posts;
         posts.forEach(element => {
-            container.innerHTML += ` <div
-        class="flex flex-col py-[20px] px-[3%] mt-[20px] mb-[20px] w-[80%] bg-primary-light rounded-md"
+            container.innerHTML += ` <div class="flex flex-col py-[20px] px-[3%] mt-[20px] max-md:mt-[40px] mb-[20px] w-[100%] bg-primary-mid dark:bg-text-mid rounded-md"
        id="content">
         <div class="flex flex-row items-start gap-[10px]">
           <image
@@ -15,23 +14,23 @@ async function appendBlog() {
             class="w-[55px] h-[55px] object-cover bg-secondary-main rounded-[50%]"
           />
           <div>
-            <h2 class="font-semibold text-sm text-text-main">Prajwol Neupane</h2>
-            <h4 class="font-medium text-xxs text-text-light">
+            <h2 class="font-semibold text-sm text-text-main dark:text-primary-light">Prajwol Neupane</h2>
+            <h4 class="font-medium text-xxs text-text-light dark:text-primary-main">
               prajwolneupane68@gmail.com
             </h4>
           </div>
         </div>
-        <h1 class="font-medium text-lg text-text-main">
+        <h1 class="font-medium text-lg text-text-main dark:text-primary-light">
          ${element.title}
         </h1>
         <p
-          class="font-medium text-xxs text-transparent bg-gradient-to-b from-text-mid via-text-mid to-primary-light bg-clip-text"
+          class="font-regular text-xxs text-transparent bg-gradient-to-b from-text-mid via-text-mid to-primary-light bg-clip-text dark:text-primary-mid"
         >
         ${element.body}
         </p>
         <a href="https://tailwindcss.com/docs/min-height" target="_blank" class="mt-[-35px] w-[200px]  mx-[auto]">
           <button
-            class="bg-text-light  py-[8px] w-[100%] text-xxs font-regular text-white  rounded-md hover:bg-text-mid"
+            class="bg-text-light dark:bg-primary-mid  py-[8px] w-[100%] text-xxs font-regular text-white dark:text-text-mid rounded-md hover:bg-text-mid dark:hover:bg-primary-light"
           >
             Continue Reading
           </button>
