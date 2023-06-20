6b22f1ae4c66802eb0b69d2fa732e7ffa2f2acd7
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-06-20 23:39:59 +0545

    Create User Added

--- index.js
+++ index.js
@@ -2,45 +2,75 @@ import axios from 'axios';
 import express from 'express';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
+import cookieParser from 'cookie-parser';
 
 
 const app = express();
+app.use(cookieParser());
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.set('view engine', 'ejs')
 app.use(express.static(__dirname + '/public'));
 
-app.get("/",async function(req,res) {
-    try{
-        console.log(req.cookies);
-        const response = await axios("https://dummyjson.com/posts");
+app.get("/", async function (req, res) {
+    try {
+        var response = await axios("https://dummyjson.com/posts");
         const data = response.data.posts;
-        res.render("HomePage",{data:data});
-    }catch(e){
+        var user = null
+        if (req.cookies.token) {
+            response = await axios.post("http://localhost:8000/api/v2/user/auth", {
+                token: req.cookies.token
+            }, {
+                headers: {
+                    'api_key': "mero-54321-app"
+                }
+            });
+            user = response.data;
+        }
+        res.render("HomePage", { data: data, user: user });
+    } catch (e) {
         res.json({
-            error:"Error"
+            error: "Error"
         });
         console.log(e);
     }
 
 })
-app.get("/blog/:id",async function(req,res) {
-    try{
+app.get("/blog/:id", async function (req, res) {
+    try {
 
-    }catch(e){
+    } catch (e) {
         console.log(e);
     }
 })
 
-app.get("/login",async function(req,res){
+app.get("/login", async function (req, res) {
     res.render("LoginPage");
 })
 
-app.get("/signup",async function(req,res){
+app.get("/signup", async function (req, res) {
     res.render("SignUpPage");
 })
 
-app.get("/about",async function(req,res){
+app.get("/profile", async function (req, res) {
+    if (req.cookies.token) {
+        try {
+            const response = await axios.post("http://localhost:8000/api/v2/user/auth", {
+                token: req.cookies.token
+            }, {
+                headers: {
+                    'api_key': "mero-54321-app"
+                }
+            });
+            const user = response.data;
+            res.render("ProfilePage", { user: user });
+        } catch (e) {
+            console.log(e);
+        }
+    }
+});
+
+app.get("/about", async function (req, res) {
     res.render("AboutPage");
 });
 
--- public/js/script.js
+++ public/js/script.js
@@ -3,6 +3,15 @@
 var openFilterModal = true;
 var isDarkTheme = localStorage.getItem("theme") ? localStorage.getItem("theme") == "true" : false;
 
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+function deleteCookie(cookieName) {
+    document.cookie = cookieName + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+}
 function filterModalAction() {
     if (openFilterModal) {
         document.getElementById("filtermodal").classList.remove("hidden");
@@ -55,17 +64,79 @@ async function logIn() {
             toast.innerHTML = `
         <h2 class="text-xs font-semibold text-text-mid">Log In Fail</h2>
           <h4 class="text-xxs font-medium text-text-mid">${data.message}</h4>`
-          setTimeout(()=>{
-            toast.classList.add("bottom-[-100px]");
-            toast.classList.remove("bottom-[50px]")
-          },3500)
+            setTimeout(() => {
+                toast.classList.add("bottom-[-100px]");
+                toast.classList.remove("bottom-[50px]")
+            }, 3500)
         }
-        if(data.token){
-            document.cookie = `token=${data.token}`
+        if (data.token) {
+            setCookie("token", data.token, 30);
             window.location.href = "/"
         }
     } catch (e) {
         console.log(e);
 
+    }
+}
+// Log Out
+async function logOut() {
+    deleteCookie("token");
+    window.location.href = "/"
+}
+// Sign Up
+async function signUp() {
+
+    var name = document.getElementById("signup-name").value
+    var email = document.getElementById("signup-email").value
+    var password = document.getElementById("signup-password").value
+    var cpassword = document.getElementById("signup-cpassword").value
+    var toast = document.getElementById("login-toast");
+    if (password == cpassword) {
+        try {
+            const response = await fetch(`http://localhost:8000/api/v2/user/createUser`, {
+                method: 'POST',
+                headers: {
+                    Accept: 'application.json',
+                    'Content-Type': 'application/json',
+                    'api_key': "mero-54321-app"
+                },
+                body: JSON.stringify({
+                    name: name,
+                    email: email,
+                    password: password
+                })
+            });
+            const data = await response.json();
+            if (data.message) {
+                toast.classList.remove("bottom-[-100px]");
+                toast.classList.add("bottom-[50px]")
+                toast.classList.add("border-fail")
+                toast.innerHTML = `
+        <h2 class="text-xs font-semibold text-text-mid">Log In Fail</h2>
+          <h4 class="text-xxs font-medium text-text-mid">${data.message}</h4>`
+                setTimeout(() => {
+                    toast.classList.add("bottom-[-100px]");
+                    toast.classList.remove("bottom-[50px]")
+                }, 3500)
+            }
+            if (data.token) {
+                setCookie("token", data.token, 30);
+                window.location.href = "/"
+            }
+        } catch (e) {
+            console.log(e);
+
+        }
+    } else {
+        toast.classList.remove("bottom-[-100px]");
+        toast.classList.add("bottom-[50px]")
+        toast.classList.add("border-fail")
+        toast.innerHTML = `
+        <h2 class="text-xs font-semibold text-text-mid">Sign in fail</h2>
+          <h4 class="text-xxs font-medium text-text-mid">Confirm Password didn't match</h4>`
+        setTimeout(() => {
+            toast.classList.add("bottom-[-100px]");
+            toast.classList.remove("bottom-[50px]")
+        }, 3500)
     }
 }
