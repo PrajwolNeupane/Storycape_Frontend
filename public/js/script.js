77d5cf1326f831072e4a1cba304da1ac65742350
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-06-20 10:00:42 +0545

    Adding Dark and Light Mode

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,7 @@
-
+//Global Variables
 var openFilterModal = true;
+var isDarkTheme = localStorage.getItem("theme") ? localStorage.getItem("theme") == "true" :false ;
+
 
 function filterModalAction () {
     if(openFilterModal){
@@ -12,3 +14,17 @@ function filterModalAction () {
         openFilterModal = true;
     }
 }
+
+function changeTheme (theme) {
+    if(!theme){   
+        document.documentElement.classList.remove('dark')
+        document.getElementById("sun-icon").classList.add("hidden");
+        document.getElementById("moon-icon").classList.remove("hidden");
+    }else{
+        document.documentElement.classList.add('dark')
+        document.getElementById("sun-icon").classList.remove("hidden");
+        document.getElementById("moon-icon").classList.add("hidden");
+    }
+    localStorage.setItem("theme",theme)
+}
+changeTheme(isDarkTheme);
